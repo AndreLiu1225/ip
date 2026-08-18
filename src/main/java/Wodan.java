cb8d82276d6baa129f6b0c6e3f49876995207c73
AndreLiu1225 <andreliu2004@gmail.com>
2026-08-18 21:09:37 +0800

    Level 2. Add, List

--- src/main/java/Wodan.java
+++ src/main/java/Wodan.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -6,6 +7,8 @@ import java.util.Scanner;
  */
 public class Wodan {
     public static void main(String[] args) {
+
+
         String banner = " __          __       _\n"
                 + " \\ \\        / /      | |\n"
                 + "  \\ \\  /\\  / /__   __| | __ _ _ __\n"
@@ -15,10 +18,14 @@ public class Wodan {
 
         String line = "    ____________________________________________________________";
 
+        // Array to store tasks
+        ArrayList<String> tasks = new ArrayList<String>();
+        int idx = 0;
+
         System.out.println(banner);
         System.out.println(line);
-        System.out.println("     Hello! I'm Wodan.");
-        System.out.println("     What can I do for you?");
+        System.out.println("     Hail, wanderer. Wodan is listening.");
+        System.out.println("     What is your command?");
         System.out.println(line);
         System.out.println();
         Scanner in = new Scanner(System.in);
@@ -26,14 +33,24 @@ public class Wodan {
             String command = in.nextLine();
             if (command.equals("bye")) {
                 System.out.println(line);
-                System.out.println("     Bye. Hope to see you again soon!");
+                System.out.println("     So it is written. Farewell, wanderer.");
                 System.out.println(line);
                 break;
+            }
+            if (command.equals("list")) {
+                System.out.println(line);
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.printf("     %d. %s%n", i + 1, tasks.get(i));
+                }
+                System.out.println(line);
+                continue;
             }
             System.out.println(line);
             System.out.println("     " + command);
             System.out.println(line);
             System.out.println();
+
+            tasks.add(command);
         }
     }
 }
